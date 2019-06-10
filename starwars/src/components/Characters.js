f82fd3a5b21b5c551677075372a7a7ee90740842
Cobrettie <cobrettiegarner3@gmail.com>
2019-06-10 18:48:21 -0400

    update list to complete MVP

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -4,6 +4,8 @@ const Characters = props => {
     return (
         <div className="character-container">
             <div className="character-font">{props.starwarsChars[props.index].name}</div>
+            <div className="character-font2">Height: {props.starwarsChars[props.index].height}</div>
+            <div className="character-font2">Mass: {props.starwarsChars[props.index].mass}</div>
         </div>
     );
 };
